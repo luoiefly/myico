954b09de51772b65aa1d0ac49ff30939e5e93fc2
luoie <luoiefly@163.com>
2018-02-02 20:46:27 +0800

    modify TokenERC20

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-var TokenERC20Advanced = artifacts.require("./TokenERC20Advanced.sol");
+var TokenERC20 = artifacts.require("./TokenERC20.sol");
 
 module.exports = function(deployer) {
     const initialSupply = 200000; 
     const tokenName = "MyCoin";
     const tokenSymbol = "%";
-    deployer.deploy(TokenERC20Advanced, initialSupply, tokenName, tokenSymbol);
+    deployer.deploy(TokenERC20, initialSupply, tokenName, tokenSymbol);
 };
